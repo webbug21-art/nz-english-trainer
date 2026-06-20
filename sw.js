b320e8d1a876e56dc66d12a8ac64a0f502f0a51b
webbug21-art <webbug21@gmail.com>
2026-06-21 07:19:05 +1200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nz-self-thinking-v2";
+const CACHE_NAME = "nz-self-thinking-v3";
 const APP_FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
